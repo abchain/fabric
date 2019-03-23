231590c8a07ac716c815ca6e11644acb071cc3a1
qzhongwood <2609611@qq.com>
2019-03-23 14:58:18 +0800

    fix temp ledger bug

--- core/statesync/block_test.go
+++ core/statesync/block_test.go
@@ -2,44 +2,151 @@ package statesync
 
 import (
 	"fmt"
+	_ "github.com/abchain/fabric/core/config"
 	"github.com/abchain/fabric/core/ledger"
+	"github.com/abchain/fabric/core/ledger/testutil"
 	"github.com/abchain/fabric/core/util"
+	"github.com/abchain/fabric/flogging"
 	pb "github.com/abchain/fabric/protos"
-	"github.com/spf13/viper"
 	"golang.org/x/net/context"
-	"io/ioutil"
 	"os"
 	"testing"
 )
 
-func TestMain(m *testing.M) {
+type itest interface {
+	call()
+}
 
-	fmt.Printf("func TestMain\n")
-	setupTestConfig()
+type base struct {
+	aa int
+}
+
+func (b *base) call()  {
+	fmt.Printf("base call\n")
+}
+
+type child struct {
+	*base
+}
+
+//func (b *child) call()  {
+//	fmt.Printf("child call\n")
+//}
+
+
+
+//func TestMain(m *testing.M) {
+//	setupTestConfig()
+//	os.Exit(m.Run())
+//}
+
+
+func TestMain(m *testing.M) {
+	testutil.SetupTestConfig()
 	os.Exit(m.Run())
 }
 
-func setupTestConfig() {
-	tempDir, err := ioutil.TempDir("", "fabric-db-test")
-	if err != nil {
-		panic(err)
+
+
+func TestBaseChild(t *testing.T) {
+	var obj itest
+	obj = &child{}
+
+	obj.call()
+}
+
+func formTestData(t *testing.T, ledger *ledger.Ledger, height int) {
+
+	uuid := util.GenerateUUID()
+	transaction, _ := pb.NewTransaction(pb.ChaincodeID{Path: "testUrl"}, uuid, "anyfunction", []string{"param1, param2"})
+
+	//add gensis block
+	ledger.BeginTxBatch(0)
+	ledger.CommitTxBatch(0, nil, nil, nil)
+	for ib := 1; ib < height; ib++ {
+		ledger.BeginTxBatch(ib)
+		ledger.TxBegin("txUuid")
+		ledger.SetState("chaincode1", "keybase", []byte{byte(ib)})
+		ledger.SetState("chaincode2", "keybase", []byte{byte(ib)})
+		ledger.SetState("chaincode3", "keybase", []byte{byte(ib)})
+		ledger.TxFinished("txUuid", true)
+		ledger.CommitTxBatch(ib, []*pb.Transaction{transaction}, nil, []byte("proof1"))
+		size := ledger.GetBlockchainSize()
+		stateDelta, _ := ledger.GetStateDelta(size - 1)
+		if(stateDelta == nil) {
+			t.Fatalf("Get unexpected GetStateDelta result")
+		}
 	}
-	viper.Set("peer.fileSystemPath", tempDir)
-	deleteTestDBPath()
+
+	statehash, err := ledger.GetCurrentStateHash()
+	if(err != nil) {
+		t.Fatalf("Get unexpected GetStateDelta result")
+	}
+
+	t.Logf("Target statehash: %x", statehash)
+}
+
+
+
+func TestBlockSync(t *testing.T) {
+
+	testLedger := ledger.InitTestLedger(t)
+
+	clientMockSyncHandler := newMockSyncHandler2(&pb.PeerID{Name: "server"}, testLedger)
+	client := newTestClient(testLedger, clientMockSyncHandler)
+	clientMockSyncHandler.client = client
+
+	height := 2019
+	formTestData(t, testLedger, height)
+
+	serverMockSyncHandler := newMockSyncHandler2(&pb.PeerID{Name: "client"}, testLedger)
+	server := newTestServer(testLedger, serverMockSyncHandler)
+	serverMockSyncHandler.server = server
+
+	for i := 1; i < height; i++ {
+
+		stateDelta, err := server.ledger.GetStateDelta(uint64(i))
+		if err != nil {
+			t.Fatalf("[%s]: Failed to get state detals. err: %s", flogging.GoRDef, err)
+		}
+		if stateDelta == nil {
+			t.Fatalf("[%s]: Invalid state detals. height: %d", flogging.GoRDef, i)
+		}
+	}
+
+	clientMockSyncHandler.peer = serverMockSyncHandler
+	serverMockSyncHandler.peer = clientMockSyncHandler
+
+
+	//mk := server.parent.(mockSyncHandler2)
+
+
+	go clientMockSyncHandler.handleServerChat()
+	go serverMockSyncHandler.handleClientChat()
+
+	_, endBlockNumber, err := client.getSyncTargetBlockNumber()
+
+	if err != nil {
+		t.Fatalf("[%s]: Failed to sync state detals. err: %s", flogging.GoRDef, err)
+	}
+
+	clientMockSyncHandler.fsmHandler.Event(enterGetDelta)
+	msgHandler := newBlockMessageHandler(2, endBlockNumber, client)
+	client.syncMessageHandler = msgHandler
+	err = client.executeSync() // sync block cf
+	if err != nil {
+		t.Fatalf("[%s]: Failed to sync state detals. err: %s", flogging.GoRDef, err)
+	}
 }
 
 
 func newTestServer(l *ledger.Ledger, parent ISyncHandler) (*stateServer) {
-
 	s := &stateServer{
 		parent: parent,
 	}
 	s.ledger = l.CreateSnapshot()
 
-	stateDelta, _ := s.ledger.GetStateDelta(2)
-
-	_ = stateDelta
-
+	s.parent.sendSyncMsg(nil,9,nil)
 	return s
 
 }
@@ -51,99 +158,9 @@ func newTestClient(l *ledger.Ledger, parent ISyncHandler) (*syncer) {
 		parent: parent,
 		Context: context.TODO(),
 	}
+
+	sts.syncMessageChan = make(chan *pb.SyncMessage)
+	sts.positionResp = make(chan *pb.SyncStateResp)
+	sts.startResponseChan = make(chan *pb.SyncStartResponse)
 	return sts
-}
-
-
-func formTestData(ledger *ledger.Ledger) {
-	transaction, _ := buildTestTx()
-	//add gensis block
-	ledger.BeginTxBatch(0)
-	ledger.CommitTxBatch(0, nil, nil, nil)
-	for ib := 0; ib < 5; ib++ {
-		ledger.BeginTxBatch(1)
-		ledger.TxBegin("txUuid")
-		ledger.SetState("chaincode1", "keybase", []byte{byte(ib)})
-		ledger.SetState("chaincode2", "keybase", []byte{byte(ib)})
-		ledger.SetState("chaincode3", "keybase", []byte{byte(ib)})
-		ledger.TxFinished("txUuid", true)
-		ledger.CommitTxBatch(1, []*pb.Transaction{transaction}, nil, []byte("proof1"))
-	}
-
-}
-
-
-
-func buildTestTx() (*pb.Transaction, string) {
-	uuid := util.GenerateUUID()
-	tx, _ := pb.NewTransaction(pb.ChaincodeID{Path: "testUrl"}, uuid, "anyfunction", []string{"param1, param2"})
-	return tx, uuid
-}
-
-
-func TestBlockSync(t *testing.T) {
-
-	tl := ledger.InitTestLedger(t)
-
-	clientMockSyncHandler := newMockSyncHandler(&pb.PeerID{Name: "server"}, tl)
-	client := newTestClient(tl, clientMockSyncHandler)
-	clientMockSyncHandler.client = client
-
-	formTestData(tl)
-
-
-	serverMockSyncHandler := newMockSyncHandler(&pb.PeerID{Name: "client"}, tl)
-	server := newTestServer(tl, serverMockSyncHandler)
-	serverMockSyncHandler.server = server
-
-
-	clientMockSyncHandler.peer = serverMockSyncHandler
-	serverMockSyncHandler.peer = clientMockSyncHandler
-
-	go clientMockSyncHandler.handleServerChat()
-	go serverMockSyncHandler.handleClientChat()
-
-	//clientMockSyncHandler.fsmHandler.Event(enterSyncBegin)
-	clientMockSyncHandler.fsmHandler.Event(enterGetDelta2)
-	serverMockSyncHandler.fsmHandler.Event(enterServe)
-
-	msgHandler := newBlockMessageHandler(2, 5, client)
-	client.syncMessageHandler = msgHandler
-	err := client.executeSync() // sync block cf
-	if err != nil {
-		logger.Errorf("[%s]: Failed to sync state detals. err: %s", err)
-	}
-}
-
-
-func assertTrue(t *testing.T, v bool) {
-	if v {
-		return
-	}
-
-	t.Fatalf("Get unexpected FALSE result")
-}
-
-func assertIntEqual(t *testing.T, v int, exp int) {
-	if v == exp {
-		return
-	}
-
-	t.Fatalf("Value is not equal: get %d, expected %d", v, exp)
-}
-
-func assertByteEqual(t *testing.T, v []byte, expected string) {
-	if expected == "" && v == nil {
-		return
-	} else if string(v) == expected {
-		return
-	}
-
-	t.Fatalf("Value is not equal: get %s, expected %s", string(v), expected)
-}
-
-
-func deleteTestDBPath() {
-	dbPath := viper.GetString("peer.fileSystemPath")
-	os.RemoveAll(dbPath)
 }
--- core/statesync/handler.go
+++ core/statesync/handler.go
@@ -15,8 +15,6 @@ var enterSyncFinish = "SyncFinish"
 var enterGetState = "GetState"
 var enterServe = "Serve"
 
-var enterGetDelta2 = "enterGetDelta2"
-
 
 func newFsmHandler(h ISyncHandler) *fsm.FSM {
 
@@ -31,8 +29,6 @@ func newFsmHandler(h ISyncHandler) *fsm.FSM {
 			//serving phase
 			{Name: pb.SyncMsg_SYNC_SESSION_START.String(),        Src: []string{"idle"},  Dst: "serve"},
 			{Name: pb.SyncMsg_SYNC_SESSION_QUERY.String(),        Src: []string{"serve"}, Dst: "serve"},
-			//{Name: pb.SyncMsg_SYNC_SESSION_GET_BLOCKS.String(),   Src: []string{"serve"}, Dst: "serve"},
-			//{Name: pb.SyncMsg_SYNC_SESSION_GET_SNAPSHOT.String(), Src: []string{"serve"}, Dst: "serve"},
 			{Name: pb.SyncMsg_SYNC_SESSION_DELTAS.String(),   Src: []string{"serve"}, Dst: "serve"},
 			{Name: pb.SyncMsg_SYNC_SESSION_SYNC_MESSAGE.String(),   Src: []string{"serve"}, Dst: "serve"},
 			{Name: pb.SyncMsg_SYNC_SESSION_END.String(),          Src: []string{"serve"}, Dst: "idle"},
@@ -40,8 +36,6 @@ func newFsmHandler(h ISyncHandler) *fsm.FSM {
 			//client phase
 			{Name: pb.SyncMsg_SYNC_SESSION_START_ACK.String(), Src: []string{"synchandshake"}, Dst: "synclocating"},
 			{Name: pb.SyncMsg_SYNC_SESSION_QUERY_ACK.String(),  Src: []string{"synclocating"},  Dst: "synclocating"},
-			//{Name: pb.SyncMsg_SYNC_SESSION_BLOCKS.String(),    Src: []string{"syncblock"},     Dst: "syncblock"},
-			//{Name: pb.SyncMsg_SYNC_SESSION_SNAPSHOT.String(),  Src: []string{"syncsnapshot"},  Dst: "syncsnapshot"},
 			{Name: pb.SyncMsg_SYNC_SESSION_DELTAS_ACK.String(),    Src: []string{"syncdelta"},     Dst: "syncdelta"},
 			{Name: pb.SyncMsg_SYNC_SESSION_SYNC_MESSAGE_ACK.String(),    Src: []string{"synclocating", "syncdelta", "syncstate"},     Dst: "syncstate"},
 
@@ -50,13 +44,11 @@ func newFsmHandler(h ISyncHandler) *fsm.FSM {
 
 			// client
 			{Name: enterSyncBegin,   Src: []string{"idle"}, Dst: "synchandshake"},
-			{Name: enterGetDelta2,   Src: []string{"idle"}, Dst: "syncdelta"},
 			{Name: enterGetBlock,    Src: syncPhase,        Dst: "syncblock"},
 			{Name: enterGetSnapshot, Src: syncPhase,        Dst: "syncsnapshot"},
 			{Name: enterGetDelta,    Src: syncPhase,        Dst: "syncdelta"},
 			{Name: enterGetState,    Src: syncPhase,        Dst: "syncstate"},
 			{Name: enterSyncFinish,  Src: syncPhase,        Dst: "idle"},
-
 		},
 		fsm.Callbacks{
 			// for both server and client
@@ -69,19 +61,16 @@ func newFsmHandler(h ISyncHandler) *fsm.FSM {
 			// server
 			"before_" + pb.SyncMsg_SYNC_SESSION_START.String():        func(e *fsm.Event) { h.beforeSyncStart(e) },
 			"before_" + pb.SyncMsg_SYNC_SESSION_QUERY.String():        func(e *fsm.Event) { h.getServer().beforeQuery(e) },
-			//"before_" + pb.SyncMsg_SYNC_SESSION_GET_BLOCKS.String():   func(e *fsm.Event) { h.getServer().beforeGetBlocks(e) },
-			"before_" + pb.SyncMsg_SYNC_SESSION_DELTAS.String():   func(e *fsm.Event) { h.getServer().beforeGetDeltas(e) },
+			"before_" + pb.SyncMsg_SYNC_SESSION_DELTAS.String():       func(e *fsm.Event) { h.getServer().beforeGetDeltas(e) },
 			"before_" + pb.SyncMsg_SYNC_SESSION_END.String():          func(e *fsm.Event) { h.getServer().beforeSyncEnd(e) },
-			"before_" + pb.SyncMsg_SYNC_SESSION_SYNC_MESSAGE.String():    func(e *fsm.Event) { h.getServer().beforeSyncMessage(e) },
+			"before_" + pb.SyncMsg_SYNC_SESSION_SYNC_MESSAGE.String(): func(e *fsm.Event) { h.getServer().beforeSyncMessage(e) },
 
 			"leave_serve":                                             func(e *fsm.Event) { h.getServer().leaveServe(e) },
 			"enter_serve":                                             func(e *fsm.Event) { h.getServer().enterServe(e) },
 
 			// client
-			"after_" + pb.SyncMsg_SYNC_SESSION_START_ACK.String(): func(e *fsm.Event) { h.getClient().afterSyncStartResponse(e) },
+			"after_" + pb.SyncMsg_SYNC_SESSION_START_ACK.String():  func(e *fsm.Event) { h.getClient().afterSyncStartResponse(e) },
 			"after_" + pb.SyncMsg_SYNC_SESSION_QUERY_ACK.String():  func(e *fsm.Event) { h.getClient().afterQueryResponse(e) },
-			//"after_" + pb.SyncMsg_SYNC_SESSION_BLOCKS.String():    func(e *fsm.Event) { h.getClient().afterSyncBlocks(e) },
-			//"after_" + pb.SyncMsg_SYNC_SESSION_DELTAS_ACK.String():    func(e *fsm.Event) { h.getClient().afterSyncStateDeltas(e) },
 			"after_" + pb.SyncMsg_SYNC_SESSION_SYNC_MESSAGE_ACK.String():    func(e *fsm.Event) { h.getClient().afterSyncMessage(e) },
 
 			"leave_synclocating":                                  func(e *fsm.Event) { h.getClient().leaveSyncLocating(e) },
