a1b28b28cc2090dfcc71f9d90b04faa42cab57bd
qzhongwood <2609611@qq.com>
2019-04-20 22:26:34 +0800

    test scripts

--- examples/txnetwork/setdebug.sh
+++ examples/txnetwork/setdebug.sh
@@ -10,9 +10,7 @@ function query {
     ((LOCADDRPORT = PEERLOCALADDRBASE + $1 * 100))
     export CORE_SERVICE_CLIADDRESS=127.0.0.1:${LOCADDRPORT}
 
-#    ${PEER_BINARY} network status
-    ${PEER_BINARY} chaincode query -n txnetwork -c "{\"Function\": \"debug\", \"Args\": [\"on\"]}"
-#    ${PEER_BINARY} chaincode query -n txnetwork -c "{\"Function\": \"status\", \"Args\": []}"
+    ${PEER_BINARY} chaincode query -n txnetwork -c "{\"Function\": \"debug\", \"Args\": [\"$2\"]}"
 }
 
-query $1
+query $1 $2
