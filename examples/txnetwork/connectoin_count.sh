7a508bef943362587518ff4e0ddb0dceed5e25e1
qzhongwood <2609611@qq.com>
2019-04-21 12:40:49 +0800

    test scripts

--- examples/txnetwork/connectoin_count.sh
+++ examples/txnetwork/connectoin_count.sh
@@ -3,10 +3,12 @@
 
 main() {
 
-    for ((index=0; index<$2; index++)) do
-        res=`./run.sh $1 |grep "ESTABLISHED"| wc -l`
+    num_peer=$1
+    ((num_connection=num_peer * num_peer - num_peer))
+    for ((index=0; index<${2}; index++)) do
+        res=`./run.sh ${num_peer} |grep "ESTABLISHED"| wc -l`
 
-        if [ $res -lt 12 ] && [ $res -gt 0 ]; then
+        if [ $res -lt ${num_connection} ] && [ $res -gt 0 ]; then
             echo "unexpected: $res"
             exit
         fi
