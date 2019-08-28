2607c6d35b93a1ce73e38f6224ea966f4677a0dc
Wei Fan <noel.wei@gmail.com>
2019-08-28 16:32:09 +0800

    chaincodesupport: use random txid on executelite, so there will be no duplicated Tx error on concurrent query

--- protos/init.go
+++ protos/init.go
@@ -16,11 +16,48 @@ limitations under the License.
 
 package protos
 
-import "github.com/op/go-logging"
+import (
+	"crypto/rand"
+	"encoding/binary"
+	mrand "math/rand"
+	"time"
+
+	"github.com/op/go-logging"
+)
 
 var logger *logging.Logger
 
 func init() {
 	// Create package logger.
 	logger = logging.MustGetLogger("protos")
+}
+
+func MustGetUUID() (uuid [16]byte) {
+	_, err := rand.Read(uuid[:])
+	if err != nil {
+
+		logger.Warningf("Use weak uuid for error in random driver: %s", err)
+
+		//use a uuid-1 like variant
+		tm := time.Now()
+		tmpart := tm.Unix()
+		tmpartExt := tm.UnixNano()
+		rnpart := mrand.Int63()
+
+		binary.LittleEndian.PutUint64(uuid[:], uint64(tmpart))
+		binary.BigEndian.PutUint64(uuid[8:], uint64(rnpart))
+		binary.BigEndian.PutUint16(uuid[8:], uint16(tmpartExt))
+
+		// variant-2 (microsoft) bits
+		uuid[8] = uuid[8]&^0xe0 | 0xc0
+		// version 1 (pseudo-random); see section 4.1.3
+		uuid[6] = uuid[6]&^0xf0 | 0x10
+	} else {
+		// variant bits; see section 4.1.1
+		uuid[8] = uuid[8]&^0xc0 | 0x80
+
+		// version 4 (pseudo-random); see section 4.1.3
+		uuid[6] = uuid[6]&^0xf0 | 0x40
+	}
+	return
 }
